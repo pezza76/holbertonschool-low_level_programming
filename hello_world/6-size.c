d9d8329331ebfdf91116f20886f71ba229705df3
Andrew Perry <pezza76@hotmail>
2025-02-17 23:50:38 +0000

    printf

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -9,9 +9,9 @@ int main(void)
 	float e;
 
 	printf("Size of a char: %zu byte(s)", sizeof(a));
-	printf("Size of a char: %zu byte(s)", sizeof(b));
-	printf("Size of a char: %zu byte(s)", sizeof(c));
-	printf("Size of a char: %zu byte(s)", sizeof(d));
-	printf("Size of a char: %zu byte(s)", sizeof(e));
+	printf("Size of a int: %zu byte(s)", sizeof(b));
+	printf("Size of a long int: %zu byte(s)", sizeof(c));
+	printf("Size of a long long int: %zu byte(s)", sizeof(d));
+	printf("Size of a float: %zu byte(s)", sizeof(e));
 	return (0);
 }
