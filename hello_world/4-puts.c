3466ed681d61424b52bced5fed1feb6a0c0e0246
Andrew Perry <pezza76@hotmail>
2025-02-17 08:01:32 +0000

    betty

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,11 @@
 
 #include <stdio.h>
 
+/**
+ * main function - Prints  "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
