f914ffd66e4c55c6408e41db1433eb98cf729165
Andrew Perry <pezza76@hotmail>
2025-02-17 22:34:01 +0000

    sizeof

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	
+	printf("Size of a char: %c byte(s)", sizeof(a));
+	printf("Size of a char: %d byte(s)", sizeof(b));
+	printf("Size of a char: %ld byte(s)", sizeof(c));
+	printf("Size of a char: %lld byte(s)", sizeof(d));
+	printf("Size of a char: %f byte(s)", sizeof(e);
+	return(0);
+}
