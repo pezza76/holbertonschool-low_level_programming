e911c6f28710170337c1663957af6375c9943eae
Andrew Perry <pezza76@hotmail>
2025-02-18 11:40:44 +0000

    q8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (ch = 'a', ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+	return (0);
+}
