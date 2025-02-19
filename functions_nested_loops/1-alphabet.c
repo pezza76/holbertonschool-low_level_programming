2b100ea6c484e84956deec6b7ce364ce3127fc15
Andrew Perry <pezza76@hotmail>
2025-02-19 17:55:11 +0000

    q1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+*/
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
