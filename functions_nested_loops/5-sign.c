ae9ca754e43eea174b97a615263846c9417b27af
Andrew Perry <pezza76@hotmail>
2025-02-20 12:19:11 +0000

    q5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio>
+
 
 /**
  * print_sign - prints a sign
@@ -7,22 +7,22 @@
  *
  * Return: int
 */
- 
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar('+');
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		putchar('-');
+		_putchar('-');
 		return (-1);
 	}
 }
