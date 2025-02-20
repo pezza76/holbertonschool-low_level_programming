6084783c1d6e339aca87d69926ec587ee12e5073
Andrew Perry <pezza76@hotmail>
2025-02-20 06:19:05 +0000

    q3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a character is lowercase
+ *
+ * Return: Int
+*/
+
+int _islower(int c)
+{
+
+	if (islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
