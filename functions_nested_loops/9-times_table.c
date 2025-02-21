b9aa7174671b39f7b2e0b40d3dabdb4d92958f10
Andrew Perry <pezza76@hotmail>
2025-02-21 13:18:12 +0000

    q9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -27,7 +27,7 @@ void times_table(void)
 	  }
           _putchar(r % 10 + '0');
           
-          if (j != 9)
+          if (j != 0)
           {
           _putchar(',');
           _putchar('.');
