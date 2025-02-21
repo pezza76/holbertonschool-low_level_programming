634494ae37a5ab16ee065c3dd651fba26a2c056d
Andrew Perry <pezza76@hotmail>
2025-02-21 02:35:52 +0000

    q8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: void
+*/
+
+void jack_bauer(void)
+{
+	for (int i = 0; i < 24; i++)
+	{
+		for (z = 0; z < 60; z++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(z / 10 + '0');
+			_putchar(z % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
