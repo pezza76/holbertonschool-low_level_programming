ff06209685ed10cd36d91d489609836b26fa9f70
Andrew Perry <pezza76@hotmail>
2025-02-21 21:59:51 +0000

    q9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,33 +7,33 @@
 */
 
 void times_table(void)
-{	
-	  int i;
-  int j;
-  
-  for (i = 0; i < 10; i++)
-  {
-      for (j = 0; j < 10; j++)
-      {
-          int r = i * j;
-          
-          if (r > 9)
-          {
-            _putchar(r / 10 + '0');
-          }
-	  if (r <= 9 && j > 0)
-	  {
-		  _putchar(' ');
-	  }
-          _putchar(r % 10 + '0');
-          
-          if (j != 9)
-          {
-          _putchar(',');
-          _putchar(' ');
-          }
-          
-      }
-      _putchar('\n');
-  }
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int r = i * j;
+
+			if (r > 9)
+			{
+				_putchar(r / 10 + '0');
+			}
+			if (r <= 9 && j > 0)
+			{
+				_putchar(' ');
+			}
+		_putchar(r % 10 + '0');
+
+		if (j != 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+
+	}
+		_putchar('\n');
+	}
 }
