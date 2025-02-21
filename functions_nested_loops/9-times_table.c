4fdb4ea8cde37b132d5d63156deece0625098c72
Andrew Perry <pezza76@hotmail>
2025-02-21 04:32:25 +0000

    q9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * times_table - return 9 times table
+ *
+ * Return: void
+*/
+
+void times_table(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			result = i * j;
+			if (result > 9)
+				_putchar(result / 10 + '0');
+
+			_putchar(result % 10 + '0');
+			if (j == 9)
+			break;
+			_putchar(',');
+		}
+	_putchar('\n');
+
+}
