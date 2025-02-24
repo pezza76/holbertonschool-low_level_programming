fdc044e379a526545a7206752a85f09275a8e1a8
Andrew Perry <pezza76@hotmail>
2025-02-24 03:00:09 +0000

    q0

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *_isupper - returns true if character is upper case
+ *
+ *@c: integer
+ *Return: int
+*/
+
+int _isupper(int c)
+{
+       if (_issupper(c))
+       {
+               return (1);
+       }
+       else
+       {
+              return (0);
+        }
+}
