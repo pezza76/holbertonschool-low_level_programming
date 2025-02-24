bdb798b29247651e39c4f7c05e618ce4cc38bb7d
Andrew Perry <pezza76@hotmail>
2025-02-24 09:41:18 +0000

    fizzbuzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ *main - prints numbers 1-100 and replaces some numbers with Fizz, Buzz or Fizbuzz
+ *
+ *Return: void
+*/
+
+
+int main(void)
+{
+	for (i = 1; i < 101; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			print("FizzBuzz/n");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+}
