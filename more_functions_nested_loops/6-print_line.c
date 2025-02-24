1214423a93c6b9e736f724c752ec809895d42a37
Andrew Perry <pezza76@hotmail>
2025-02-24 05:34:23 +0000

    q6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_line - prints a line n times
+ *
+ *Return: int
+*/
+
+void print_line(int n)
+{
+	while (n > 0)
+	{
+		_putchar('_');
+		n--;
+	}
+	_putchar('\n');
+}
