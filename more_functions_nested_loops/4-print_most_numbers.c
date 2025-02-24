878f07b320881864cb1433b8de6412b57e06081e
Andrew Perry <pezza76@hotmail>
2025-02-24 04:29:15 +0000

    q4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - print digits 0-9 except for 2 and 4
+ *
+ *Return: void
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i + '0');
+		}
+	}
+	_putchar('\n');
+}
