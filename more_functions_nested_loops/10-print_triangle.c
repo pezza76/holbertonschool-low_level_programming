43924e6b28b863f9519de4fdf64c8258395ebe61
Andrew Perry <pezza76@hotmail>
2025-02-24 11:25:06 +0000

    tri

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ *print_triangle - prints a triangle
+ *
+ * @size: int
+ *Return: int
+*/
+
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		puchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+		}
+	}
+}
