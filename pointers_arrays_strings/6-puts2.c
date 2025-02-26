feb3f0c32d05d32b4a00f5908f1a317221cba2a8
Andrew Perry <pezza76@hotmail>
2025-02-26 08:30:29 +0000

    puts

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,21 +1,25 @@
 #include "main.h"
 
 /**
- *puts2 - prints every second character
- *
- *@str: pointer
- *Return: void
- *
-*/
-
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
 void puts2(char *str)
 {
-	char *i = str;
+	int len, i;
 
-	for ( ; *i != '\0'; i+=2)
+	len = 0;
+
+	while (str[len] != '\0')
 	{
-		_putchar(*i);
+		len++;
 	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+
 	_putchar('\n');
 }
-			
