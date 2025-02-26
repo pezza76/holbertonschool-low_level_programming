7c3764904f2457893e9e5b72ed2b4ddf6596bafa
Andrew Perry <pezza76@hotmail>
2025-02-26 06:54:15 +0000

    pointer

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every second character
+ *
+ *@str: pointer
+ *Return: void
+ *
+*/
+
+void puts2(char *str)
+{
+	char *i = str;
+
+	for ( ; *i != '\0'; i+=2)
+	{
+		_putchar(*i);
+	}
+}
+			
