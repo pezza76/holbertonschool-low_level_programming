62bc328cbbd4af7489677092e66c5e44bc3b7790
Andrew Perry <pezza76@hotmail>
2025-02-26 10:09:39 +0000

    half

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *puts_half - prints first half of string
+ *
+ *@str: pointer
+ *Return: void
+*/
+
+
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (len % 2 == 0 && i < len / 2)
+		{
+
+			putchar(str[i]);
+		}
+		else if (i <= len / 2)
+		{
+			putchar(str[i]);
+		}
