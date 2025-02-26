644ddb95767687312aaf8e20480ee92e2799d37a
Andrew Perry <pezza76@hotmail>
2025-02-26 22:19:33 +0000

    q8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+i
+/**
+ *print_array - function that prints n elements of an array of integers
+ *
+ *@a: pointer
+ *@n: int
+ *Return: void
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n-1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
