2ee0d27cb2715b33091d1811136f1733a4cc937d
Andrew Perry <pezza76@hotmail>
2025-02-27 22:56:30 +0000

    string concat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *_strncat - function that concatenates two strings
+ *
+ *@dest: destination string
+ *@src: source string
+ *@n: int
+ *Return: pointer
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int count = 0;
+	*original_dest = dest;
+
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+
+	while (*src != '\0' && count < n)
+	{
+		*dest = *src;
+		count++;
+		src++;
+		dest++;
+	}
+
+	return (original_dest);
+}
+
