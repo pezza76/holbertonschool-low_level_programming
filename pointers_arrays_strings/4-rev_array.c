63a12d056c50bff4673db508f3ab9adf6001a381
Andrew Perry <pezza76@hotmail>
2025-02-28 06:17:05 +0000

    reverse array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *reverse_array - reverses an aray
+ *
+ *@a: pointer
+ *@n: pointer
+ *Return: void
+*/
+
+
+void reverse_array(int *a, int n)
+{
+	int *x = a;
+	int i = 0;
+	int j;
+
+	for ( ; i < n-1; i++)
+	{
+		x++;
+	}
+
+	for (j = 0;  j < n/2; j++)
+	{
+			int temp = *a;
+			*x = *a;
+			*x = temp;
+			x--;
+			a++;
+		}
+	}
+	return (0);
+}
+
