cef7b963113b5cab3e6dd5d5a0ebebc69d2da5cb
Andrew Perry <pezza76@hotmail>
2025-02-28 12:44:39 +0000

    toupper

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *string_toupper - converts lowercase to uppercase
+ *
+ *@str: a string
+ *Return: char
+*/
+
+char *string_toupper(char str*);
+{
+	char *x = str;
+	int i = 0;
+	int len = 0;
+
+	while (*x != '\0')
+	{
+		len++;
+		x++;
+	}
+	 
+	for (; i < len; i++)
+	{
+		if (*str >= 'a' && *str <= 'z')
+		{
+			*str = *str - 32;
+			str++;
+		}
+	}
+}
