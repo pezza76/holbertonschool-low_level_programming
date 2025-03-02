39828c7ce7291ec693c9bf0a67f3c3d4c5889711
Andrew Perry <pezza76@hotmail>
2025-03-02 00:56:34 +0000

    caps

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ *cap_string - capitalizes each word
+ *
+ *@str: pointer to a string
+ *Return: pointer
+*/
+
+char *cap_string(char str*)
+{
+	char *x = str;
+
+
+    while (*x != '\0')
+    {
+        if (*x == ' ' || *x == ' ' ||*x == '\t ' ||*x == '\n ' ||*x == ',' ||*x == ';' ||*x == '.' ||*x == '!' ||*x == '"' ||*x == '(' ||*x == ')' ||*x == '{' ||*x == '}')
+        {
+            x++;
+            continue;
+        }
+        if ((*x >= 'a' && *x <= 'z') || *x >= ('A' && *x <= 'Z' ))
+        {
+            if (*x >= 'a' && *x <= 'z')
+                    *x = *x - 32;
+        }
+        x++;
+        while (*x == ' ' || *x == ' ' ||*x == '\t ' ||*x == '\n ' ||*x == ',' ||*x == ';' ||*x == '.' ||*x == '!' ||*x == '"' ||*x == '(' ||*x == ')' ||*x == '{' ||*x == '}')
+        {
+            x++;
+        }
+
+
+    }
+
+	return (str);
+
+}
