553edcead9e536b1b07269ec8e373319cafda840
Andrew Perry <pezza76@hotmail>
2025-03-02 04:00:28 +0000

    caps

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,32 +9,32 @@
 
 char *cap_string(char *str)
 {
-    char *x = str;
-    int is_new_word = 1;
+	char *x = str;
+	int is_new_word = 1;
 
-    while (*x != '\0')
-    {
-        
-        if (*x == ' ' || *x == '\t' || *x == '\n' || *x == ',' || *x == ';' || *x == '.' || *x == '!' || *x == '"' || *x == '(' || *x == ')' || *x == '{' || *x == '}')
-        {
-            is_new_word = 1;
-        }
-        else
-        {
-           
-            if (is_new_word && *x >= 'a' && *x <= 'z')
-            {
-                *x = *x - 32;  
-                is_new_word = 0;
-            }
-            else
-            {
-                is_new_word = 0;
-            }
-        }
+	while (*x != '\0')
+	{
 
-        x++;
-    }
+		if (*x == ' ' || *x == '\t' || *x == '\n' || *x == ',' || *x == ';' || *x == '.' || *x == '!' || *x == '"' || *x == '(' || *x == ')' || *x == '{' || *x == '}')
+		{
+			is_new_word = 1;
+		}
+		else
+		{
 
-    return str;
+			if (is_new_word && *x >= 'a' && *x <= 'z')
+			{		
+				*x = *x - 32;
+				is_new_word = 0;
+			}
+			else
+			{
+				is_new_word = 0;
+			}
+		}
+
+	x++;
+	}
+
+return (str);
 }
