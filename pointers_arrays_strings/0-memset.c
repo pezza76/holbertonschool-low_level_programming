fbcd5233cf06714ef9f6796f35948102670912c7
Andrew Perry <pezza76@hotmail>
2025-03-03 12:56:25 +0000

    mem

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- *_memset - function fills the first n bytes of the memory area pointed to by s with the constant byte b
+ *_memset - function fills first n bytes of the memory pointed to by s with  b
  *
  *@b: character
  *@n: int
  *Return: pointer
 */
 
- char *_memset(char *s, char b, unsigned int n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
