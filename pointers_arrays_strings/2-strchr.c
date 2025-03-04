fb53133d02008ef0c667abadf46e57df188cc5d9
Andrew Perry <pezza76@hotmail>
2025-03-04 11:45:36 +0000

    strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ *_strchr - finds first occurence of c in s
+ *
+ *@s: pointer
+ *@c: character
+ *Return: pointer
+*/
+
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+			
+		}
+		s++;
+	}
+
+		return (NULL);
+}	
+
