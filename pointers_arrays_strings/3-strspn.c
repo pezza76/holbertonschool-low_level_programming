2976d51ed1af842f1304978375b071237fb4de94
Andrew Perry <pezza76@hotmail>
2025-03-06 13:53:32 +0000

    strspn

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -17,14 +17,21 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		int match = 0;
+
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
-				len++;
+				match = 1;
 				break;
 			}
 		}
+		if (match == 0)
+		{
+			break;
+		}
+		len++;
 	}
 				
 	return (len);
