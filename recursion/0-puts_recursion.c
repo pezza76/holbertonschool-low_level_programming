f29499d8a852a34ae93074e2066b259add598eb1
Andrew Perry <pezza76@hotmail>
2025-03-11 07:29:28 +0000

    puts

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,7 +11,7 @@ void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		
+
 		_putchar(*s);
 
 		_puts_recursion(s + 1);
@@ -20,7 +20,7 @@ void _puts_recursion(char *s)
 	}
 	else
 	{
-		
+
 		_putchar('\n');
 	}
 }
