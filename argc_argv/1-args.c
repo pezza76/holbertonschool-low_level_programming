64f6840616d9658fc35f8486d4c5ecaa10e3d36b
Andrew Perry <pezza76@hotmail>
2025-03-12 21:43:15 +0000

    argc

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *main - Program that prints the number of arguments passed to it
+ *
+ *@argc: number of arguments passed
+ *@argv: arguments
+ *Return: 0 on success
+*/
+
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
