44c85127de5c6448bb1ccc3e444a3c881ec44900
Andrew Perry <pezza76@hotmail>
2025-03-13 01:27:34 +0000

    2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - a program that prints all arguments it receives
+ *
+ *@argc: int
+ *@argv: strings
+ *Return: 0 on success
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+
+}
