926bad6308dca4f6fd033915ab9e76e30b89b43e
Andrew Perry <pezza76@hotmail>
2025-03-18 21:27:21 +0000

    exit

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *malloc_checked - function that allocates memory using malloc
+ *
+ *@b: unsigned int
+ *Return: pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+
+	void *ptr;
+
+	ptr = malloc(b);
+
+
+
+	if (ptr  == NULL)
+	{
+		exit(98);
+	}
+
+
+	return (ptr);
+}
+
+
