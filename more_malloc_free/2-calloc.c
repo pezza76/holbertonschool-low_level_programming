128431b77c95d58f4cf676972eb09012169bf1ff
Andrew Perry <pezza76@hotmail>
2025-03-19 11:10:04 +0000

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *calloc - similar to calloc function
+ *
+ *@nmemb: unsigned int
+ *@size: unsigned int
+ *Return: void pointer
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int total_size;
+	int i;
+
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	total_size = nmemb * size;
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb * size; i++)
+	{
+		(i(char *)ptr)[i] = 0;
+	}
+
+	return (ptr);
+}
+
