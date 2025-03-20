2792e37a5b1c9efae500ad3a79a54c82b18f898a
Andrew Perry <pezza76@hotmail>
2025-03-20 06:12:00 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+
+
+/**
+ *struct - dog
+ *@name: pointer to a char
+ *@age: pointer to a float
+ *@owner: pointer to a char
+*/
+
+	struct dog 
+	{
+
+		char *name;
+		float age;
+		char *owner;
+	}
+
+
