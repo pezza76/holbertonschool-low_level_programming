8887231e575431dfda1d42d721ee9a080dd6ae0a
Andrew Perry <pezza76@hotmail>
2025-03-22 05:28:51 +0000

    struct

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,6 +1,6 @@
-#include "dog.h"
-#include <stdlib.h>
+
 #include <stdio.h>
+#include "dog.h"
 
 /**
  *init_dog - a function that initialize a variable of type struct dog
@@ -15,9 +15,8 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
-		return;
-	}
+	return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
