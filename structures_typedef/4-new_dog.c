0151daa3f96510aae91b5caa39ce4daeb0165744
Andrew Perry <pezza76@hotmail>
2025-03-24 05:14:48 +0000

    4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ *new_dog -  function that creates a new dog
+ *
+ *@name: char pointer
+ *@age: float
+ *@owner: char pointer
+*/
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	int name_len = 0, owner_len = 0, i;
+	
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	
+	while (name[name_len])
+		name_len++;
+
+	while (owner[owner_len])
+    	owner_len++;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
+
+	new_dog->name = malloc(name_len + 1);
+	if (new_dog->name == NULL)
+	{
+    		free(new_dog);
+    		return (NULL);
+	}
+
+	for (i = 0; i <= name_len; i++)
+		new_dog->name[i] = name[i];
+
+	for (i = 0; i <= owner_len; i++)
+		new_dog->owner[i] = owner[i];
+
+	new_dog->age = age;
+	
+	return (new_dog);
+}
+
+
+
+
+
