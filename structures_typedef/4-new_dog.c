4f799da7ca275cd0e9473beed012d11f278c197d
Andrew Perry <pezza76@hotmail>
2025-03-24 05:20:47 +0000

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -37,6 +37,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i <= name_len; i++)
 		new_dog->name[i] = name[i];
 
+	new_dog->owner = malloc(owner_len + 1);
+	if (new_dog->owner == NULL)
+	{
+    		free(new_dog->name);
+    		free(new_dog);
+    		return (NULL);
+	}
+
 	for (i = 0; i <= owner_len; i++)
 		new_dog->owner[i] = owner[i];
 
