2ec607531d7e8cee93662ad7e0027760342f0b6b
Andrew Perry <pezza76@hotmail>
2025-03-24 05:26:13 +0000

    4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,21 +7,22 @@
  *@name: char pointer
  *@age: float
  *@owner: char pointer
+ *Return: pointer
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 	int name_len = 0, owner_len = 0, i;
-	
+
 	if (name == NULL || owner == NULL)
 		return (NULL);
-	
+
 	while (name[name_len])
 		name_len++;
 
 	while (owner[owner_len])
-    	owner_len++;
+	owner_len++;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
@@ -30,8 +31,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc(name_len + 1);
 	if (new_dog->name == NULL)
 	{
-    		free(new_dog);
-    		return (NULL);
+		free(new_dog);
+		return (NULL);
 	}
 
 	for (i = 0; i <= name_len; i++)
@@ -40,16 +41,16 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(owner_len + 1);
 	if (new_dog->owner == NULL)
 	{
-    		free(new_dog->name);
-    		free(new_dog);
-    		return (NULL);
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
 	}
 
 	for (i = 0; i <= owner_len; i++)
 		new_dog->owner[i] = owner[i];
 
 	new_dog->age = age;
-	
+
 	return (new_dog);
 }
 
