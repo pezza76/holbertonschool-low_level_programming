0857ab462038a63d3e788629c2cc958fd4a635b8
Andrew Perry <pezza76@hotmail>
2025-03-25 10:08:52 +0000

    1

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,16 +2,23 @@
 #include <stddef.h>
 
 /**
+ *array_iterator - function
  *
- *
- *
- *
- *
+ *@array: pointer
+ *@size: size_t
+ *action: pointer function that takes int as parameter
+ *Return: void
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t size;
+
 	if (array == NULL || action == NULL)
 		return;
 
-
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
