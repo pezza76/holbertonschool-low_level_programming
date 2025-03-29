b64f0d785da1c1d67a98eee575fdca0aebb1d947
Andrew Perry <pezza76@hotmail>
2025-03-29 00:45:51 +0000

    2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,41 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ *print_strings - prints strings with a separator inbetween
+ *
+ *@separator: string
+ *@n: unsigned int
+ *Return: void
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+	char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, char*);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+
+			printf("%s", str);
+		}
+
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+
+	printf("\n");
