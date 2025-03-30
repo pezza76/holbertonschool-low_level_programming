0405593f46399bf35e35d9965d1ac2c050ade310
Andrew Perry <pezza76@hotmail>
2025-03-30 01:04:10 +0000

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -17,28 +17,33 @@ void print_all(const char * const format, ...)
 	int num;
 	double num2;
 	char *str;
+	int first = 1;
 
 	va_start(args, format);
 
 	while (format[i] != '\0')
 	{
+		if (first == 0)
+		{
+			printf(", ");
+		}
 		switch (format[i])
 		{
 			case 'c':
 				c = (char)va_arg(args, int);
 
-				printf(", %c", c);
+				printf("%c", c);
 				break;
 			case 'i':
 				num = va_arg(args, int);
 
-				printf(", %i", num);
+				printf("%i", num);
 				break;
 			case 'f':
 			{
 				num2 = va_arg(args, double);
 
-				printf(", %f", num2);
+				printf("%f", num2);
 				break;
 			}
 			case 's':
@@ -51,11 +56,12 @@ void print_all(const char * const format, ...)
 					break;
 				}
 				
-					printf(", %s", str);
+					printf("%s", str);
 					break;
 			}
 			
 		}
+		first = 0;
 		i++;
 	}
 	va_end(args);
