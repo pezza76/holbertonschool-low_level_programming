b853a092d215c3af0fbe4fbcc051ebb821223b90
Andrew Perry <pezza76@hotmail>
2025-03-30 02:28:10 +0000

    3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -21,7 +21,7 @@ void print_all(const char * const format, ...)
         
                 if (format == NULL)
         {
-                
+		printf('\0');
                 return;
         }
         va_start(args, format);
