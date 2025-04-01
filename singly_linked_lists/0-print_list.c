5d577923c325e15e9064897e92193289d5f25532
Andrew Perry <pezza76@hotmail>
2025-04-01 21:38:20 +0000

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,12 +3,26 @@
 #include "lists.h"
 
 /**
+ *print_list - prints all the elements of a list_t list
  *
- *
- *
- *
- *
- *
+ *@h: pointer to a struct
+ *Return: the number of nodes
 */
 
-size_t print_list(const list_t *h);
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] ", h->len);
+
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("%s\n", h->str);
+		h = h->next;
+		count++;
+	}
+	return(count);
+}
