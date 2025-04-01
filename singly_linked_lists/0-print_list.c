3b844cb59445311467c143ed4444aaf086e0b349
Andrew Perry <pezza76@hotmail>
2025-04-01 22:26:01 +0000

    0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -15,12 +15,11 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		printf("[%u] ", h->len);
 
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("%s\n", h->str);
+			printf("%u %s\n", h->len,  h->str);
 		h = h->next;
 		count++;
 	}
