503dc22e3eb671072a616c5facf9da0594a3a388
Andrew Perry <pezza76@hotmail>
2025-04-07 00:59:02 +0000

    3

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -17,8 +17,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
-
+	
+	if (new_node == NULL)
+	{
+		return(NULL):
+	}
 	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
 	for(len = 0; str[len] != '\0'; len++);
 
@@ -31,5 +41,23 @@ list_t *add_node_end(list_t **head, const char *str)
 	else
 	{
 		list_t *current = *head;
+
+		while (current->next != '\0')
+		{
+			current->next = new_node;
+		}
+	}
+	
+	new_node->next = NULL;
+
+	if (new_node == NULL)
+	{
+		return(NULL);
+	}
+	else
+	{
+		return (new_node);
+	}
+}
 
 
