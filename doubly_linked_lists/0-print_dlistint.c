8eaac481404115e60e083627d9452cafb1cd5420
Andrew Perry <pezza76@hotmail>
2025-04-07 12:09:53 +0000

    0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -3,28 +3,22 @@
 #include "lists.h"
 
 /**
+ *print_dlistint - prints all elements of a list
  *
- *
- *
- *
- *
- *
+ *@h: pointer to a struct
+ *Return: size_t number
 */
 
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	dlistint_t *new_node;
-	
-	new_node = malloc(sizeof(dlistint_t));
-
-	new_node->n;
-	new_node->prev;
-	new_node->next;
-
-	if (head == NULL)
+	if (h != NULL)
 	{
-		head = new_node;
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
 	}
+
+	return(count);
 }
