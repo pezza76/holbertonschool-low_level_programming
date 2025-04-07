00a9c21b7ce86c3a1c9ba05a63a15f4cfc7f0697
Andrew Perry <pezza76@hotmail>
2025-04-07 06:38:32 +0000

    0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	dlistint_t *new_node;
+	
+	new_node = malloc(sizeof(dlistint_t));
+
+	new_node->n;
+	new_node->prev;
+	new_node->next;
+
+	if (head == NULL)
+	{
+		head = new_node;
+	}
+}
