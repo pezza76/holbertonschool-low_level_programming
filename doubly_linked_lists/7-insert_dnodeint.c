4c44868f95c2c3ae6e651b5b24f97bbd77ac1076
Andrew Perry <pezza76@hotmail>
2025-04-08 23:48:50 +0000

    7

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *insert_dnodeint_at_index - function that inserts a new node at a given position
+ *
+ *@idx: unsigned int
+ *@n: int
+ *Return: struct pointer
+*/
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
