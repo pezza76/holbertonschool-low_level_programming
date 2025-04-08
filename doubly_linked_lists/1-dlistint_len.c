ead46f64dd19ba39f5ca6cad0a43b8604b5187f2
Andrew Perry <pezza76@hotmail>
2025-04-08 03:52:43 +0000

    1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ *dlistint_len - returns the number of elements
+ *
+ *@h: pointer 
+ *Return: size_t number
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
