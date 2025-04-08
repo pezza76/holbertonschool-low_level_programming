dbc690b33a413d4f372488b550e763d78a76cfaf
Andrew Perry <pezza76@hotmail>
2025-04-08 23:42:49 +0000

    6

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -3,16 +3,33 @@
 #include "lists.h"
 
 /**
+ *sum_dlistint - function that returns the sum of all the data of a linked list
  *
- *
- *
- *
- *
+ *@head: struct pointer
+ *Return: int
 */
 
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
 
+	if (head == NULL)
+	{
+		return (0);
+	}
 
+	if (head->next == NULL)
+	{
+		sum = sum + head->n;
+		return (sum);
+	}
+
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+
+	return (sum);
 
 }
