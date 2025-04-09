b5bfdda821d5daf5dd1d5d6b26ce6e81e070924a
Andrew Perry <pezza76@hotmail>
2025-04-09 04:14:04 +0000

    7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,4 +10,48 @@
  *Return: struct pointer
 */
 
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *temp = *h;
+	dlistint_t *new_node;
+	unsigned int i;
+
+	if (h == NULL)
+		return(NULL);
+	
+	if (new_node == NULL)
+		return (NULL);
+	
+	if (idx == 0)
+	{
+		return (add_dnodeint(h, n));
+	}
+
+
+
+		for (i = 0; temp != NULL && i < idx - 1; i++)
+		temp = temp->next;
+
+	if (temp == NULL)
+		return (NULL); // Index out of range
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n)); // Add at the end
+
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	new_node->next = temp->next;
+	new_node->prev = temp;
+
+	temp->next->prev = new_node;
+	temp->next = new_node;
+
+	return (new_node);
+}
+
+
