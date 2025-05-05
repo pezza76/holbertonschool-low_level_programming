77dc5af208ace568d69d04fd8bfd80f5397473e5
Andrew Perry <pezza76@hotmail.com>
2025-05-05 11:42:30 +0000

    task1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,28 +7,28 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int fd, bytes_written;
+	int fd, bytes_written;
 
-    if (filename == NULL)
-	return -1;
+	if (filename == NULL)
+	return (-1);
 
-    
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-    
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+
 	if (fd == -1)
-		return -1;
+		return (-1);
 
-	if (text_content != NULL) 
+	if (text_content != NULL)
 	{
 		bytes_written = write(fd, text_content, strlen(text_content));
 		if (bytes_written == -1)
-        {
+		{
 			close(fd);
-			return -1;
-        }
-    }
+			return (-1);
+		}
+	}
 
 	close(fd);
-	return 1;
+	return (1);
 }
 
