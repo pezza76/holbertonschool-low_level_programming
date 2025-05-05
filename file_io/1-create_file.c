274007308646c1849c8f644728919047c0b20f6e
Andrew Perry <pezza76@hotmail.com>
2025-05-05 11:36:58 +0000

    task1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+    int fd, bytes_written;
+
+    if (filename == NULL)
+	return -1;
+
+    
+    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    
+	if (fd == -1)
+		return -1;
+
+	if (text_content != NULL) 
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+        {
+			close(fd);
+			return -1;
+        }
+    }
+
+	close(fd);
+	return 1;
+}
+
